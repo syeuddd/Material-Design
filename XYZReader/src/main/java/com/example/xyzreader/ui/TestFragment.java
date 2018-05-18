47047dd123abf06aa5d46e9af5b881f0987404cd
Syed Ehteshamuddin <ehteshamuddin.syed@gmail.com>
2018-05-18 16:36:50 -0600

    added all missing files

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/TestFragment.java
@@ -0,0 +1,23 @@
+package com.example.xyzreader.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ehteshs1 on 2018/05/16.
+ */
+
+public class TestFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+}
